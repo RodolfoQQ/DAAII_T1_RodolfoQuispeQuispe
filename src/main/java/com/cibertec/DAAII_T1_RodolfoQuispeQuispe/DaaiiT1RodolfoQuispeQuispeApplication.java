255447f4217641fd75b6237682ed51f3d0a160a3
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-06-07 12:14:52 -0500

    primer comit proyecto creado

---
+++ src/main/java/com/cibertec/DAAII_T1_RodolfoQuispeQuispe/DaaiiT1RodolfoQuispeQuispeApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAAII_T1_RodolfoQuispeQuispe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1RodolfoQuispeQuispeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1RodolfoQuispeQuispeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/DAAII_T1_RodolfoQuispeQuispe/DaaiiT1RodolfoQuispeQuispeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAAII_T1_RodolfoQuispeQuispe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaiiT1RodolfoQuispeQuispeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
